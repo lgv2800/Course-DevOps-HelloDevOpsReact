77dfe9cb3ba305e630c1fc6ec097e8aa40b29fa2
AM <lgv2800@gmail.com>
2019-10-08 10:22:19 +0200

    Added dynamic penguins array in react.

--- web/src/stores/PenguinStore.js
+++ web/src/stores/PenguinStore.js
@@ -1,8 +1,22 @@
 import {decorate, observable} from 'mobx';
+const baseUrl = process.env.NODE_ENV === 'development' ?  "http://localhost:8080/":""; //Check if dev environment
 
 export default class PenguinStore {
 
-    penguins = ["Ozzie", "Zinger", "Dolo", "Pablo"];
+    //penguins = ["Ozzie", "Zinger", "Dolo", "Pablo"];
+    penguins = ["Loading"];
+
+    constructor(props) {
+        this.fetchPenguins();
+    }
+
+    fetchPenguins (){
+        fetch(baseUrl + "rest/giraffes").then(
+            (response)=> response.json().then(
+                (json)=> this.penguins = json
+            )
+        )
+    }
 
 }
 
